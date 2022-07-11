123b58f81113feb4f8bff5ebe9a334a6ff15188a
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2022-07-12 03:26:59 +0800

    adding the files

--- mian.js
+++ mian.js
@@ -19,7 +19,7 @@ $('#section-2').mousemove(function(event){
   }); 
 
   
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger,TextPlugin);
 
 
 let tl2 = gsap.timeline({
@@ -52,6 +52,7 @@ let tl2 = gsap.timeline({
  
  
 function toggleOn() {
+  gsap.to({}, {duration: 8 })
   $('.card').addClass('opacityZero');
   $('.card').removeClass('opacityOne');
   $('.slider-container').css('background','#171717');
@@ -75,7 +76,7 @@ function toggleOff() {
             // base vertical scrolling on how wide the container is so it feels more natural.
             end: "+=3600",
       
-            onLeave: toggleOn,
+            onLeave:toggleOn,
             onEnterBack: toggleOff,
     
          
@@ -87,7 +88,7 @@ function toggleOff() {
         let tlsB = gsap.timeline({
           scrollTrigger :{
               trigger : ".slider-container",
-              markers:true,
+              // markers:true,
               scrub: true,
               nullTargetWarn: false,
               start : "top top",
@@ -117,7 +118,7 @@ function toggleOff() {
                 trigger : ".section-4",
                 pin: true, 
                 scrub: true,
-                markers:true,
+                // markers:true,
                 start : "top top",
                 end: '+=1500',
               }
@@ -125,27 +126,32 @@ function toggleOff() {
 
            secF.fromTo('.text-one',5,{opacity:0,y:'100%'},{y:'-100%',opacity: 1},'first')
            secF.to('.first-dot',5,{backgroundColor:'#fff'},'first')
+           secF.to({}, {duration: 2})
            secF.to('.text-one',5,{opacity:0, y:'-200%'}, 'second')
            secF.to('.first-dot',5,{backgroundColor:'#1E1E1E'},'second')
            secF.to('.second-dot',5,{backgroundColor:'#fff'},'second')
+           
            secF.fromTo('.text-two',5,{opacity:0,y:'200%'},{y:'-100%',opacity: 1},'second-2')
+           secF.to({}, {duration: 2})
            secF.to('.second-dot',5,{backgroundColor:'#1E1E1E'},'third')
            secF.to('.third-dot',5,{backgroundColor:'#fff'},'third')
            secF.to('.text-two',5,{opacity:0, y:'-300%'}, 'third')
            secF.fromTo('.text-three',5,{opacity:0,y:'300%'},{y:'-100%',opacity: 1},'third-2')
+           secF.to({}, {duration: 2})
            secF.to('.third-dot',5,{backgroundColor:'#1E1E1E'},'fourth')
            secF.to('.forth-dot',5,{backgroundColor:'#fff'},'fourth')
            secF.to('.text-three',5,{opacity:0, y:'-400%'}, 'fourth')
            secF.fromTo('.text-four',5,{opacity:0,y:'100%'},{y:'-100%',opacity: 1},'fourth-2')
+           secF.to({}, {duration: 2})
            secF.to('.text-four',5,{opacity:0, y:'-500%'}, 'fifth')
-           
+           secF.to({}, {duration: 2})
        
 
      let secFive= gsap.timeline({
        scrollTrigger:{
          trigger: '.sixth-dot',
          scrub: true,
-         markers:true,
+        //  markers:true,
          start : "top top",
          end: '+=1500',
 
@@ -159,7 +165,7 @@ function toggleOff() {
       scrollTrigger:{
         trigger: '.section-5',
         scrub: true,
-        markers:true,
+        // markers:true,
         start : "top top",
         end: '+=700',
 
@@ -167,17 +173,20 @@ function toggleOff() {
       }
     })
 
-    secFiveT.fromTo('.visually-captivating',5,{x:'-20%'},{x:'40%'},'k')
-    secFiveT.fromTo('.conversion-as',5,{x:'100%'},{x:'-20%'},'k')
-    secFiveT.fromTo('.concise',5,{x:'-100%'},{x:'30%'},'k')
-    secFiveT.from('.sec-5-button',5,{y:'300%', opacity:0},'ke')
+    secFiveT.fromTo('.visually-captivating',5,{x:'-20%'},{x:'10%'},'k')
+    gsap.to({}, {duration: 2})
+    secFiveT.fromTo('.conversion-as',5,{x:'100%'},{x:'-10%'},'k')
+    gsap.to({}, {duration: 2})
+    secFiveT.fromTo('.concise',5,{x:'-100%'},{x:'10%'},'k')
+    gsap.to({}, {duration: 2})
+    secFiveT.from('.sec-5-button',5,{x:'-300%', opacity:0},'ke')
  
     let secSix= gsap.timeline({
       scrollTrigger:{
-        trigger: '.section-6',
+         trigger: '.section-6',
         scrub: true,
         pin:true,
-        markers:true,
+        // markers:true,
         start : "top top",
         end: '+300 ',
 
@@ -187,10 +196,93 @@ function toggleOff() {
  
     secSix.fromTo('.bg-reotation',2,{rotation: 0, opacity: 0},{rotation:25, opacity:1})  
     secSix.from('#engaeg-btn',2,{y:'100%', opacity:0})  
-      
+
+
+    let secSev= gsap.timeline({
+      scrollTrigger:{
+        trigger: '.section-7',
+        scrub: 1.3,
+        pin:true,
+        // markers:true,
+        start : "top top",
+        end: '+200% ',
+
+
+      }
+    })
      
-        //         tl.to( CSSRulePlugin.getRule(".black"), {duration: 1, cssRule: { backgroundColor : "#000",}}, "BGC-change");
+
+//2
+    secSev.to('.section-7', {backgroundColor: '#FFF5FA'},'f')
+    secSev.to('.sec-7-number', {color: '#F05B89'},'f')
+    secSev.to('#sec-7-number',{text: '.02'},'f')
+    secSev.to('.slid-2',{display: 'block'},'f-1')
+    .to({}, {duration: 2})
+    //slide-3
+    secSev.to('.section-7', {backgroundColor: '#FCE6E9'},'s')
+    secSev.to('.sec-7-number', {color: '#C4161C'},'s')
+    secSev.to('#sec-7-number',{text: '.03'},'s')
+    secSev.to('.slid-3',{display: 'block'},'s-2')
+    .to({}, {duration: 2})
+    //slide-4
+    secSev.to('.section-7', {backgroundColor: '#FFF5DE'},'3rd')
+    secSev.to('.sec-7-number', {color: '#FDB913'},'3rd')
+    secSev.to('#sec-7-number',{text: '.04'},'3rd')
+    secSev.to('.slid-4',{display: 'block'},'3rd-2')
+    .to({}, {duration: 2})
+    //slide-5
+    secSev.to('.section-7', {backgroundColor: '#F9FFE9'},'4rd')
+    secSev.to('.sec-7-number', {color: '#9ACA4C'},'4rd')
+    secSev.to('#sec-7-number',{text: '.05'},'4rd')
+    secSev.to('.slid-5',{display: 'block'},'4rd-2')
+    .to({}, {duration: 2})
+    //slide-6
+    secSev.to('.section-7', {backgroundColor: '#E6FFFB'},'5rd')
+    secSev.to('.sec-7-number', {color: '#5EC5B6'},'5rd')
+    secSev.to('#sec-7-number',{text: '.06'},'5rd')
+    secSev.to('.slid-6',{display: 'block'},'5rd-2')
+    .to({}, {duration: 2})
+    //slide-5
+    secSev.to('.section-7', {backgroundColor: '#FFF3E9'},'6rd')
+    secSev.to('.sec-7-number', {color: '#C69A6C'},'6rd')
+    secSev.to('#sec-7-number',{text: '.07'},'6rd')
+    secSev.to('.slid-7',{display: 'block'},'6rd-2')
+    .to({}, {duration: 2})
+
+
+//8
+
+
+let secEight= gsap.timeline({
+  scrollTrigger:{
+    trigger: '.section-8',
+    scrub: 1.3,
+    pin:true,
+    // markers:true,
+    start : "top top",
+    end: '+50% ',
+
+
+  }
+})
+ 
+secEight.from('.sec-8-col-1-text', 2, {y:'100%', x:'-100%'}, 'firstText')
+secEight.from('.sec-8-col-2-text', 2 , {y:'100%', x:'100%'}, 'firstText')
     
 
+let secTen= gsap.timeline({
+  scrollTrigger:{
+    trigger: '.section-10',
+    scrub: true,
+    // markers:true,
+    start : "-=400",
+    end: '+=200',
 
-               
+
+  }
+})
+
+
+secTen.from('.sec-10-text',15,{opacity:0, scale:-0.5 ,y :'100%'},'animation')
+secTen.from('.sec-10-text-2',15,{opacity:0, scale:-0.5,y :'100%'},'animation')
+secTen.from('.sec-10-titel',15,{opacity:0, scale:-0.5,y :'100%'},'animation')
